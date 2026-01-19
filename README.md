36d73be71c6e769f220f04ce9b54f90ad15ad312
Windson Martins <windson.martins@wmsaude.com.br>
2026-01-19 16:31:55 -0300

    feat: landing page gamer com particles e links sociais

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tubalcaim
-Learn how to design your personal responsive portfolio website using HTML, CSS, and JavaScript in this step-by-step tutorial!
---
+++ js/particles.js
@@ -0,0 +1,58 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 120,
+      "density": {
+        "enable": true,
+        "value_area": 900
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 2.4,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 2.25,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    },
+    "modes": {
+      "repulse": {
+        "distance": 120,
+        "duration": 0.4
+      }
+    }
+  },
+  "retina_detect": true
+});
